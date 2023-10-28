5e46aa72e6dd1292b79c1708d3d6b4f459412e9a
meliksahin05 <meliksahin05@gmail.com>
2023-10-28 16:10:07 +0300

    mycucumber

--- src/test/java/stepdefinitions/GoogleStepDefs.java
+++ src/test/java/stepdefinitions/GoogleStepDefs.java
@@ -1,20 +1,21 @@
 package stepdefinitions;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import pages.GoogleHomePage;
 import utilities.Driver;
+
 public class GoogleStepDefs {
     GoogleHomePage googleHomePage = new GoogleHomePage();
     @Given("user is on the google home page")
     public void user_is_on_the_google_home_page() {
         Driver.getDriver().get("https://www.google.com");
-
-        try {
+//        if there is a Google cookie popup then click.
+        try{
             googleHomePage.cookiePopUp.click();
         }catch (Exception e){
-
         }
     }
     @Given("user search for iPhone")
@@ -25,5 +26,13 @@ public class GoogleStepDefs {
     public void verify_the_result_contains_i_phone() {
 //        title contains iPhone
         Assert.assertTrue(Driver.getDriver().getTitle().contains("iPhone"));
+    }
+    @Given("search for porcelain tea pot")
+    public void search_for_porcelain_tea_pot() {
+        googleHomePage.googleSearchBox.sendKeys("porcelain tea pot"+Keys.ENTER);
+    }
+    @Then("verify the result contains porcelain tea pot")
+    public void verify_the_result_contains_porcelain_tea_pot() {
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("porcelain tea pot"));
     }
 }
