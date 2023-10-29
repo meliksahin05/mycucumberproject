4a6337b8e3ead90a3f979a705677b791b8a5ed88
meliksahin05 <meliksahin05@gmail.com>
2023-10-29 18:47:20 +0200

    extented report

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,10 +4,18 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",//generates reports in the console as well
+                "html:target/cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-reports/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        monochrome = true,//makes the console reports more readable
         features = "./src/test/resources/features",//path of the features folder
         glue = "stepdefinitions",//path of the stepdefinitions folder
         dryRun = false, //generate the missing step definitions only. Do not run the existing step definitions
-        tags = "@param_feature"
+        tags = "@data_tables"
 )
 public class Runner {
 }
@@ -22,4 +30,3 @@ glue is used to give the path of the step definitions folder
 dryRun = true is used to generate missing step definitions. dryRun=false must be used for normal run.
 tags = "@iphone" is used to run specific scenarios.
  */
-
--- src/test/java/stepdefinitions/CommonStepDefs.java
+++ src/test/java/stepdefinitions/CommonStepDefs.java
@@ -2,30 +2,20 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import pages.GoogleHomePage;
 import utilities.Driver;
-import utilities.WaitUtils;
 
 public class CommonStepDefs {
-    GoogleHomePage googleHomePage = new GoogleHomePage();
     @Given("user navigates to {string}")
     public void user_navigates_to(String url) {
         Driver.getDriver().get(url);
-        WaitUtils.waitFor(2);
-        googleHomePage.cookiePopUp.click();
-        WaitUtils.waitFor(2);
-
     }
     @Then("user verifies page title contains {string}")
     public void user_verifies_page_title_contains(String expectedTitle) {
         Assert.assertTrue(Driver.getDriver().getTitle().contains(expectedTitle));
-        WaitUtils.waitFor(2);
     }
     @Then("close the application")
-    public void close_the_application() {
-
+    public void closeTheApplication() {
         Driver.closeDriver();
-        WaitUtils.waitFor(2);
     }
 }
 
