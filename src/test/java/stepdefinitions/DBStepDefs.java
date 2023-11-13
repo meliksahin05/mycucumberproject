4d2391bbd21c9fe2ea24aa44fe0b90bdb1fbddc4
meliksahin05 <meliksahin05@gmail.com>
2023-11-13 18:59:24 +0200

    getResultObject

--- src/test/java/stepdefinitions/DBStepDefs.java
+++ src/test/java/stepdefinitions/DBStepDefs.java
@@ -1,7 +1,10 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import utilities.DBUtils;
+
+import java.sql.SQLException;
 
 public class DBStepDefs {
 
@@ -9,4 +12,42 @@ public class DBStepDefs {
     public void user_connects_to_the_application_database() {
         DBUtils.createConnection();
     }
-}
+
+    @And("user gets the column {string} from the table {string}")
+    public void userGetsTheColumnFromTheTable(String column, String table) {
+//        DBUtils.executeQuery( "select * from jhi_user" ); hard coded
+        DBUtils.executeQuery( "select "+column+" from "+table+"" );
+    }
+    @And("read all the column {string} data")
+    public void readAllTheColumnData(String column) throws SQLException {
+//        go the next row
+        DBUtils.getResultset().next();
+//        getting the data
+        Object object1 = DBUtils.getResultset().getObject(column);
+        System.out.println(object1);
+//        --------------------------
+        DBUtils.getResultset().next();
+        Object object2 = DBUtils.getResultset().getObject(column);
+        System.out.println(object2);
+//        --------------------------
+        DBUtils.getResultset().next();
+        Object object3 = DBUtils.getResultset().getObject(column);
+        System.out.println(object3);
+//        --------------------------
+        DBUtils.getResultset().absolute(4);//going to the 4th row
+        Object object4 = DBUtils.getResultset().getObject(column);
+        System.out.println(object4);
+//        ---------------------------
+
+        int rowNum = 5;
+        while (DBUtils.getResultset().next()){
+            Object currentRowData = DBUtils.getResultset().getObject(column);
+            System.out.println(rowNum +" : "+ currentRowData);
+            rowNum++;
+        }
+        System.out.println("There are "+rowNum +" data in this column "+column);
+
+
+    }
+ }
+
--- src/test/resources/features/jdbc/jdbc.feature
+++ src/test/resources/features/jdbc/jdbc.feature
@@ -2,3 +2,5 @@ Feature: jdbc_feature
   @user_login
   Scenario: TC01_read_customer_login_info
     Given user connects to the application database
+    And user gets the column "*" from the table "jhi_user"
+    And read all the column "login" data
